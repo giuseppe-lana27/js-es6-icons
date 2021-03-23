f98da1aa79214b62cabbc473ab88abfb0a51740a
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-23 16:23:36 +0100

    inserita colorazione delle icone visualizzate a schermo

--- js/script.js
+++ js/script.js
@@ -117,13 +117,31 @@ const icons = [
     category: "animal"
   },
 ];
-// mostro in pagina le icone
+// Milestone 2. coloro le icone per tipo
+const colors = ['blue', 'red', 'green'];
+const categories = [];
+// metto le icone in array per catogorie
 icons.forEach((icon) => {
-  const {name, prefix, family} = icon;
+  if (categories.includes(icon.category) == false){
+    categories.push(icon.category);
+  }
+});
+console.log(categories);
+// mappo l'array per inserire i colori in base alla categoria
+const coloredIcons = icons.map((icon) =>{
+  const catIndex = categories.indexOf(icon.category);
+  const itemColor = colors[catIndex];
+  icon.color = itemColor;
+  return icon;
+});
+console.log(coloredIcons);
+// Milestone 1. stampo a schermo le icone con i colori inseriti in Milestone 2. 
+coloredIcons.forEach((icon) => {
+  const {name, prefix, family, color} = icon;
   const htmlIcons = `
   <div>
-    <i class="${family} ${prefix}${name}"></i>
+    <i class="${family} ${prefix}${name}" style="color:${color}"></i>
     <div class="title">${name}</div>
-  </div> `;  
+  </div> `;
   $(".icons").append(htmlIcons);
 });
